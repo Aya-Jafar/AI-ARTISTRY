8d12a614810011f85dec0b40e0db2bdc9c86cc15
Aya-jafar <aya.jafar002@gmail.com>
2023-09-07 02:14:06 +0300

    adding tabs navigation & context api

---
+++ src/providers/TabContent.jsx
@@ -0,0 +1,14 @@
+import React, { Children, createContext, useState } from "react";
+
+const TabContext = createContext();
+
+export function TabContentProvider({ children }) {
+  const [currentTab, setCurrentTab] = useState("All");
+  return (
+    <TabContext.Provider value={{ currentTab, setCurrentTab }}>
+      {children}
+    </TabContext.Provider>
+  );
+}
+
+export default TabContext;
