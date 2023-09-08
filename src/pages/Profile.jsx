a9384f387de4c2fb666edc0b510b331d0f819a56
Aya-jafar <aya.jafar002@gmail.com>
2023-09-08 05:51:38 +0300

    adding saved artworks to profile page with firebase

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,36 @@
-
-import React from 'react';
+import React, { useContext, useState, useEffect } from "react";
+import profileImg from "../images/profile-img.jpeg";
+import AuthContext from "../providers/Auth";
+import Masonry from "react-masonry-css";
+import { getSavedArtworks } from "../backend/data";
 
 function Profile() {
-    return ( <>profile</> );
+  const { currentUser } = useContext(AuthContext);
+
+  const [savedArtworks, setSavedArtworks] = useState([]);
+
+  useEffect(() => {
+    getSavedArtworks(currentUser, setSavedArtworks);
+  }, [currentUser]);
+
+  return (
+    <div className="profile">
+      <div className="profile-image">
+        <img src={profileImg} alt="" />
+      </div>
+      <Masonry
+        breakpointCols={3}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {savedArtworks.map((artwork, index) => (
+          <div key={index} className="image-container">
+            <img src={artwork.image} alt="" />
+          </div>
+        ))}
+      </Masonry>
+    </div>
+  );
 }
 
 export default Profile;
