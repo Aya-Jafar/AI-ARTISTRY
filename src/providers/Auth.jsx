5fd993d350ae8e9b8b121a98cfdd14614e3ad57a
Aya-jafar <aya.jafar002@gmail.com>
2023-09-08 05:52:21 +0300

    editing function name to prevent maximum call error

--- src/providers/Auth.jsx
+++ src/providers/Auth.jsx
@@ -1,10 +1,34 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  signInWithPopup,
+  updateProfile,
+} from "firebase/auth";
 
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
+import { serverTimestamp } from "firebase/firestore";
+import app from "../backend/config";
+
+const db = getFirestore(); // Initialize your Firestore instance
 const AuthContext = createContext();
+
+
 
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  
   const auth = getAuth();
 
   useEffect(() => {
@@ -15,8 +39,81 @@ export const AuthProvider = ({ children }) => {
     return unsubscribe;
   }, [auth]);
 
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      const myCollection = collection(db, "ai-art");
+
+      // Check if the user document already exists
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        // User doesn't exist, create a new user document
+        await setDoc(doc(db, "users", user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      console.error("Google sign-in error:", error);
+    }
+  };
+
+
+
+
+  const signInWithFacebook = async () => {
+    const provider = new FacebookAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error("Facebook sign-in error:", error);
+    }
+  };
+
+  const signUpWithEmailAndPassword = async (email, displayName, password) => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      await updateProfile(user, { displayName: displayName });
+    } catch (error) {
+      console.error("Email sign-up error:", error);
+    }
+  };
+
+  const logInWithEmailAndPassword = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error("Email sign-in error:", error);
+    }
+  };
+
+  const signOutUser = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.error("Sign-out error:", error);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ currentUser, setCurrentUser }}>
+    <AuthContext.Provider
+      value={{
+        currentUser,
+        setCurrentUser,
+        signInWithGoogle,
+        signInWithFacebook,
+        signUpWithEmailAndPassword,
+        logInWithEmailAndPassword,
+        signOutUser,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
