50e8d1e821d7e8b9a819cd71628d3ed475743cb3
Aya-jafar <aya.jafar002@gmail.com>
2023-09-10 08:23:12 +0300

    Imagine page initial layout

--- src/pages/Imagine.jsx
+++ src/pages/Imagine.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { slideAnimation } from "../utils/motion";
 
 function Imagine() {
   return (
     <>
       <motion.div
         initial={{ opacity: 0 }}
-        animate={{ opacity:1 }}
+        animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.5 }}
         className="imagine-container"
       >
         <div className="prompt-section">
-          {/* <h1>Imagine</h1> */}
-          <h1>Imagine Tomorrow, Create Today ✨</h1>
+          <motion.h1 {...slideAnimation("left")}>
+            Imagine Tomorrow, Create Today
+          </motion.h1>
           <textarea type="text" name="" id="" />
           <br />
           <button className="btn" id="imagine-btn">
