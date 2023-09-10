11832a52bbf2e9e898bdc790c289ddc1ceccb59c
Aya-jafar <aya.jafar002@gmail.com>
2023-09-10 08:23:38 +0300

    all animations

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -111,3 +111,10 @@ export const scaleOnHover = {
 export const stagger = {
   visible: { transition: { staggerChildren: 0.4 } },
 };
+
+
+
+export const textVariants = {
+  hidden: { opacity: 0, y: 10 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.2 } },
+};
