890758beea6405e78fcb19994d9602864dd000cf
Aya-jafar <aya.jafar002@gmail.com>
2023-09-12 19:34:41 +0300

    making the ArtGrid generic component

--- src/components/ArtGrid.jsx
+++ src/components/ArtGrid.jsx
@@ -1,23 +1,66 @@
-import React, { useContext, useEffect } from "react";
-import TabContext from "../providers/TabContent";
-import MainArtGrid from "./AllTab";
-import FantasyTab from "./FantasyTab";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { fadeIn, scaleOnHover } from "../utils/motion";
+import { textVariants } from "../utils/motion";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { useInView } from "react-intersection-observer";
+import { Link } from "react-router-dom";
 
-function ArtGrid() {
-  const { currentTab } = useContext(TabContext);
+function ArtGrid({ artworks }) {
+  const [hoveredArtwork, setHoveredArtwork] = useState(null);
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
 
-  const generateTabContent = () => {
-    switch (currentTab) {
-      case "All":
-        return <MainArtGrid />;
-      case "Fantasy":
-        return <FantasyTab />;
-      case "SCI-FI":
-        return <h1>SCI-FI</h1>;
-    }
-  };
-
-  return <>{generateTabContent()}</>;
+  return (
+    <>
+      {artworks && (
+        <ResponsiveMasonry
+          style={{ width: "100%" }}
+          columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+        >
+          <Masonry
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {artworks.map((artwork) => (
+              <motion.div
+                key={artwork.id}
+                className="image-container"
+                ref={ref}
+                onMouseOver={() => setHoveredArtwork(artwork)}
+                onMouseOut={() => setHoveredArtwork(null)}
+              >
+                <Link to={`/artwork/${artwork.id}`}>
+                  <motion.img
+                    src={artwork.image}
+                    alt=""
+                    initial="hidden"
+                    animate={inView ? "visible" : "hidden"}
+                    className="artwork-image"
+                    variants={fadeIn}
+                  />
+                </Link>
+                <motion.div>
+                  {hoveredArtwork === artwork && (
+                    <motion.h3
+                      className="hover-text"
+                      initial="hidden"
+                      animate={inView ? "visible" : "hidden"}
+                      variants={textVariants}
+                    >
+                      {artwork.prompt}
+                    </motion.h3>
+                  )}
+                </motion.div>
+              </motion.div>
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      )}
+    </>
+  );
 }
 
 export default ArtGrid;
