639eaeecbe7630d05ea19e5c8481e844a1a35bad
Aya-jafar <aya.jafar002@gmail.com>
2023-09-12 19:37:13 +0300

    adding ArtWorkDetails route & saving functionality

--- src/components/AllTab.jsx
+++ src/components/AllTab.jsx
@@ -1,77 +1,18 @@
-import { useInView } from "react-intersection-observer";
 import React, { useContext, useEffect, useState } from "react";
-import { getAllArtworks, saveArtwork } from "../backend/data";
-import AuthContext from "../providers/Auth";
-import { Link } from "react-router-dom";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
-import { motion } from "framer-motion";
-import { fadeIn, scaleOnHover } from "../utils/motion";
-import {textVariants } from '../utils/motion'
+import { getAllArtworks } from "../backend/data";
+import ArtGrid from "./ArtGrid";
 
 function MainArtGrid() {
-  const [hoveredArtwork, setHoveredArtwork] = useState(null);
   const [artworks, setArtworks] = useState([]);
 
   useEffect(() => {
     getAllArtworks(setArtworks);
   }, []);
 
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-    threshold: 0.2,
-  });
-
   return (
-    <>
-      {artworks && (
-        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-          <Masonry
-            className="my-masonry-grid"
-            columnClassName="my-masonry-grid_column"
-          >
-            {artworks.map((artwork) => (
-              <motion.div
-                className="image-container"
-                ref={ref}
-                onMouseOver={() => setHoveredArtwork(artwork)}
-                onMouseOut={() => setHoveredArtwork(null)}
-
-                // whileHover={{ scale: 1.05 }}
-              >
-                <Link
-                  to={`/artwork/${artwork.id}`}
-                  key={artwork.id}
-                  // onClick={() => saveArtwork(currentUser, artwork.id)}
-                >
-                  <motion.img
-                    src={artwork.image}
-                    alt=""
-                    initial="hidden"
-                    animate={inView ? "visible" : "hidden"}
-                    className="artwork-image"
-                    variants={fadeIn}
-                    // whileHover="hovered"
-                    // whileHover={scaleOnHover}
-                  />
-                </Link>
-                <motion.div>
-                  {hoveredArtwork === artwork && (
-                    <motion.h3
-                      className="hover-text"
-                      initial="hidden"
-                      animate={inView ? "visible" : "hidden"}
-                      variants={textVariants}
-                    >
-                      dssssss
-                    </motion.h3>
-                  )}
-                </motion.div>
-              </motion.div>
-            ))}
-          </Masonry>
-        </ResponsiveMasonry>
-      )}
-    </>
+    <div id="art-grid-section">
+      {artworks && <ArtGrid artworks={artworks} />}
+    </div>
   );
 }
 
--- src/pages/ArtWorkDetails.jsx
+++ src/pages/ArtWorkDetails.jsx
@@ -1,31 +1,74 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { getArtworkDetails } from "../backend/data";
 import { motion } from "framer-motion";
+import AuthContext from "../providers/Auth";
+import { saveArtwork } from "../backend/data";
+import AuthPopupContext from "../providers/AuthPopup";
 
 const ArtworkDetail = () => {
   const { id } = useParams();
   const [currentArtwork, setCurrentArtwork] = useState(null);
-  
+  const { currentUser } = useContext(AuthContext);
+  const { loginPopup, setLoginPopup } = useContext(AuthPopupContext);
+
+  const navigate = useNavigate();
+
   // Fetch the artwork details using the "id" parameter
   useEffect(() => {
     getArtworkDetails(id, setCurrentArtwork);
   }, [id]);
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
-      className="artwork-detail"
-    >
-      {/* <motion.div className="artwork-detail-img"> */}
-        {currentArtwork && <motion.img src={currentArtwork.image} alt="" style={{ width: "50%" }}/>}
-      {/* </motion.div> */}
-
-      <h1>Artwork Details</h1>
-    </motion.div>
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+        className="artwork-detail"
+      >
+        <>
+          {currentArtwork && (
+            <>
+              <motion.img
+                src={currentArtwork.image}
+                alt=""
+                className="artwork-detail-img"
+                style={{ width: "60%" }}
+              />
+              <motion.div className="artwork-info">
+                <motion.h1>{currentArtwork.model}</motion.h1>
+                <motion.h3>{currentArtwork.prompt}</motion.h3>
+                <motion.p>Prompt was created by Aya</motion.p>
+                <br />
+                <div className="artwork-detail-btns">
+                  <button className="btn" id="like-btn">
+                    Like
+                  </button>
+                  <button
+                    className="btn"
+                    onClick={() => {
+                      if (currentUser) {
+                        saveArtwork(currentUser, id);
+                      } else {
+                        // setLoginPopup(true)
+                        navigate("/login");
+                        // if (currentUser) {
+                        //   navigate(`/artwork/${id}`);
+                        // }
+                      }
+                    }}
+                  >
+                    Save
+                  </button>
+                </div>
+              </motion.div>
+            </>
+          )}
+        </>
+      </motion.div>
+    </>
   );
 };
 
