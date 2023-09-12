4df7976f346da12e3a49a979492b21efbb3c0c27
Aya-jafar <aya.jafar002@gmail.com>
2023-09-12 19:35:40 +0300

    adding fantasy & sci-fi tabs and using ArtGrid component

--- src/components/FantasyTab.jsx
+++ src/components/FantasyTab.jsx
@@ -1,9 +1,21 @@
-import React from "react";
-import { slideAnimation } from "../utils/motion";
+import React, { useState, useEffect } from "react";
+import { slideAnimation, fadeAnimation } from "../utils/motion";
 import { motion } from "framer-motion";
+import { getFantasyArtworks } from "../backend/data";
+import ArtGrid from "./ArtGrid";
 
 function FantasyTab() {
-  return <motion.h1 {...slideAnimation("left")}>hi fantasy</motion.h1>;
+  const [fantasyArtworks, setFantasyArtworks] = useState([]);
+
+  useEffect(() => {
+    getFantasyArtworks(setFantasyArtworks);
+  }, []);
+
+  return (
+    <motion.div {...slideAnimation("left")}>
+      {fantasyArtworks && <ArtGrid artworks={fantasyArtworks} />}
+    </motion.div>
+  );
 }
 
 export default FantasyTab;
