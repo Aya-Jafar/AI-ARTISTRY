0f1ed9d77504b55565af6a9275eee90f1f7fea92
Aya-jafar <aya.jafar002@gmail.com>
2023-09-29 16:52:20 +0300

    Adding image generating model functionality

---
+++ src/backend/huggingFace.js
@@ -0,0 +1,36 @@
+import { HfInference } from "@huggingface/inference";
+
+const generateArt = async (setGeneratedImage, prompt) => {
+  const huggingFace = new HfInference(process.env.REACT_APP_HF_TOKEN);
+  const model = "Lykon/art-diffusion-xl-0.9";
+
+  try {
+    const response = await huggingFace.textToImage({
+      data: prompt.trim(),
+      model: model,
+    });
+    // console.log(response);
+
+    const reader = new FileReader();
+    reader.onload =  (e) => {
+      const myDataUrl = e.target.result;
+      setGeneratedImage(myDataUrl);
+    };
+    reader.readAsDataURL(response);
+
+    // setGeneratedImage(response);
+  } catch (error) {
+    console.error("Error making API request:", error);
+  }
+};
+
+export default generateArt;
+
+
+
+// const reader = new FileReader();
+// reader.onload = function (e) {
+//   const myDataUrl = e.target.result;
+//   setOutputImage(myDataUrl);
+// };
+// reader.readAsDataURL(response);
--- src/pages/Imagine.jsx
+++ src/pages/Imagine.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 import { slideAnimation } from "../utils/motion";
+import AuthContext from "../providers/Auth";
+import { Skeleton } from "@mui/material";
+import generateArt from "../backend/huggingFace";
+import CustomizationSliders from "../components/imagine/Customization";
+import { saveGeneratedImage } from "../backend/data";
+import ImagineGrid from "../components/imagine/ImagineGrid";
+import GeneratedImage from "../components/imagine/GeneratedImage";
+
 
 function Imagine() {
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [isClicked, setIsClicked] = useState(false);
+  const [prompt, setPrompt] = useState("");
+
+  const [generatedImage, setGeneratedImage] = useState("");
+
+  const [customOptions, setCustomOptions] = useState({
+    brightness: 100,
+    contrast: 100,
+  });
+
+  // useEffect(() => {
+  //   console.log(generatedImage);
+  // }, [generatedImage]);
+
+  const hangleGenerateClick = async (e) => {
+    e.preventDefault();
+    setIsClicked(true);
+    if (currentUser) {
+      try {
+        await generateArt(setGeneratedImage, prompt);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      navigate("/login");
+    }
+  };
+
   return (
     <>
       <motion.div
@@ -16,14 +56,67 @@ function Imagine() {
           <motion.h1 {...slideAnimation("left")}>
             Imagine Tomorrow, Create Today
           </motion.h1>
-          <textarea type="text" name="" id="" placeholder="Write your prompt here..."/>
+          <textarea
+            type="text"
+            name=""
+            id=""
+            placeholder="Write your prompt here..."
+            onChange={(e) => setPrompt(e.target.value)}
+          />
           <br />
-          <button className="btn" id="imagine-btn">
+
+          <button
+            className="btn"
+            id="imagine-btn"
+            onClick={(e) => hangleGenerateClick(e)}
+          >
             Generate
           </button>
+          {generatedImage && (
+            <>
+              <CustomizationSliders
+                option="Brightness"
+                setOption={setCustomOptions}
+              />
+              <CustomizationSliders
+                option="Contrast"
+                setOption={setCustomOptions}
+              />
+              <div className="artwork-detail-btn" id="imagine-custome-btns">
+                <button className="btn">Post</button>
+                <button
+                  className="btn"
+                  onClick={() =>
+                    saveGeneratedImage(currentUser, generatedImage, prompt)
+                  }
+                >
+                  Save
+                </button>
+              </div>
+            </>
+          )}
         </div>
+
         <div className="image-section">
-          <img src="" alt="placeholder..." />
+          {!isClicked ? (
+            <ImagineGrid />
+          ) : (
+            <>
+              {generatedImage ? (
+                <GeneratedImage
+                  generatedImage={generatedImage}
+                  customOptions={customOptions}
+                />
+              ) : (
+                <Skeleton
+                  sx={{ bgcolor: "grey.700" }}
+                  variant="rectangular"
+                  width={600}
+                  height={600}
+                />
+              )}
+            </>
+          )}
         </div>
       </motion.div>
     </>
