c6b61b3f3eaffd1873d8f9a6b936fbbbe874c3a3
Aya-jafar <aya.jafar002@gmail.com>
2023-09-29 16:55:55 +0300

    isAuthenticated function & signout

--- src/backend/auth.js
+++ src/backend/auth.js
@@ -1,25 +1,26 @@
-import React, { useContext } from "react";
-import AuthContext from "../providers/Auth";
 
-// const {
-//     signInWithGoogle,
-//     signInWithFacebook,
-//     setCurrentUser,
-//     signInWithEmailAndPassword,
-//   } = useContext(AuthContext);
 
-export const handleSignIn = async (logInWithEmailAndPassword , email, password) => {
 
+
+export const isAuthenticated = (currentUser) => {
+  return currentUser ? true : false;
+};
+
+
+export const handleSignIn = async (
+  logInWithEmailAndPassword,
+  email,
+  password,
+  setErrorMessage
+) => {
   try {
-    await logInWithEmailAndPassword(email, password);
+    await logInWithEmailAndPassword(email, password, setErrorMessage);
     // Handle successful sign-up, e.g., redirect the user to a different page
   } catch (error) {
     console.error("Email sign-up error:", error);
     // Handle sign-up error, e.g., display an error message to the user
   }
 };
-
-
 
 export const handleSignUp = async (
   signUpWithEmailAndPassword,
